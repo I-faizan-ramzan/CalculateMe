46751444b95ee14f614daf5b1a4c5f1b70e2a270
Faizan Ramzan <59405231+I-faizan-ramzan@users.noreply.github.com>
2024-05-01 01:16:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function getResult() {
   const a = document.getElementById("val").value;
+  const b = document.getElementById("drate").value;
+
   const result = a - a * 0.25;
+  const dollar = result * b;
   document.getElementById("res").value = result;
+  document.getElementById("dres").value = dollar;
 }
+
+document.getElementById("btn").addEventListener("click", getResult);
